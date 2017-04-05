c072a8930a0d3fbcea1ee61b789e8a48cee8825a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-04-05 13:24:41 -0400

    Initial Commit

---
+++ Scripts/core/app.js
@@ -0,0 +1,13 @@
+// IIFE - Immediately Invoked Function Expression
+(function () {
+    function Start() {
+        Main();
+    }
+    function Update() {
+    }
+    function Main() {
+        console.log("App Started!!");
+    }
+    window.onload = Start;
+})();
+//# sourceMappingURL=app.js.map
---
+++ Scripts/core/app.ts
@@ -0,0 +1,18 @@
+// IIFE - Immediately Invoked Function Expression
+(function(){
+
+function Start() {
+  Main();
+}
+
+function Update() {
+
+}
+
+function Main() {
+  console.log("App Started!!");
+}
+
+window.onload = Start;
+
+})();
